73d7ca6d96d8d0a86352128589f23fcaf3571b32
FGBxRamel <kretschmer.raziel@gmail.com>
2020-10-04 00:28:38 +0200

    Made init

--- gui.py
+++ gui.py
@@ -1,1 +1,8 @@
- 
+import tkinter as tk
+
+class GUI:
+    def __init__(self):
+        self.fenster = tk.Tk()
+        self.fenster.configure(background = "black")
+        self.fenster.attributes("-fullscreen", True)
+        self.fenster.mainloop()
